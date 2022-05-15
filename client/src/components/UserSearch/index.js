5acbf1e2c081c2833bfa45433a0c8cb13e8447a2
cheesecake_assassin <santasebastian@yahoo.com>
2022-05-15 12:49:08 -0400

    huge progress made in frontend, need to fix minor issue, saving progress to git

--- client/src/components/UserSearch/index.js
+++ client/src/components/UserSearch/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import '../../Form.css';
 
 function UserSearch() {
   const [hasSearched, setHasSearched] = useState(false);
@@ -9,7 +10,9 @@ function UserSearch() {
   function handleSubmit(event) {
     event.preventDefault();
 
-    if (!hasSearched) { setHasSearched(true) }
+    if (!hasSearched) {
+      setHasSearched(true);
+    }
 
     let username = event.target.name.value;
     console.log(username);
@@ -27,17 +30,24 @@ function UserSearch() {
           <input
             type='text'
             name='name'
-            placeholder='Enter your twitch name...'
+            placeholder='Enter your favorite Twitch channel...'
+            autoComplete='off'
           />
         </div>
-        <button type='submit'>Submit</button>
+        <button type='submit' className='submit w-button'>
+          Submit
+        </button>
+
+        {hasSearched && (
+          <div className='results'>
+            <h3 className='channel'>
+              Twitch channel:{' '}
+              <a href={`https://www.twitch.tv/${user}`} target='_blank' rel='noreferrer noopener'>{user}</a>
+            </h3>
+            <h3 className='followers'>Follower count: <span>{followers.toLocaleString()}</span></h3>
+          </div>
+        )}
       </form>
-      {hasSearched && (
-        <div>
-          <h3>{user}</h3>
-          <h3>{followers.toLocaleString()}</h3>
-        </div>
-      )}
     </section>
   );
 }
