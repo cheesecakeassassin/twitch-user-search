63ea51e2f785ea8d25f6e1988dab3872414c2642
cheesecake_assassin <santasebastian@yahoo.com>
2022-05-15 20:45:37 -0400

    improving frontend, added header/footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import './App.css';
 import UserSearch from './components/UserSearch';
 
 function App() {
   return (
     <div>
+      <header>
+        <h1 className='title'>Twitch User Search</h1>
+      </header>
       <main>
         <UserSearch></UserSearch>
       </main>
+      <footer>
+        <p>
+          Created by{' '}
+          <a
+            href='https://github.com/cheesecakeassassin'
+            target='_blank' // Opens new tab when link is clicked
+            rel='noreferrer noopener'
+          >
+            cheesecakeassassin 🎅
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
